d22e44b021cecf0738d4314608cbed692a8c5a0d
MuhamadNurFirmansyah24 <muhamadnurfirmansyah66450@gmail.com>
2021-11-27 13:50:42 +0700

    menyelesaikan video ke 13 mengenai Searching & Pagination

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,16 +7,16 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "home") ? 'active' : '' }}" href="/">Home</a>
+          <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "about") ? 'active' : '' }}" href="/about">About</a>
+          <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "posts") ? 'active' : '' }}" href="/posts">Blog</a>
+          <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="/posts">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
+          <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
         </li>
       </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => 'about',
         "name" => "Muhamad Nur Firmansyah",
         "email" => "muhamadnurfirmansyah66450@gmail.com",
         "image" => "m.jpg"
@@ -44,20 +46,5 @@ Route::get('/categories', function() {
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
-    ]);
-});
-
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'title' =>"Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
-
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
